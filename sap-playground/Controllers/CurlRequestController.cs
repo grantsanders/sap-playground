3e37ad16816b2cd12627d3c558d2cd07c0c0c983
grantsanders <gsanders@ecs-online.com>
2023-03-08 21:22:26 -0500

    initial commit

---
+++ sap-playground/Controllers/CurlRequestController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace sap_playground.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class CurlRequestController : ControllerBase
+{
+    private static readonly string[] Summaries = new[]
+    {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+    private readonly ILogger<CurlRequestController> _logger;
+
+    public CurlRequestController(ILogger<CurlRequestController> logger)
+    {
+        _logger = logger;
+    }
+
+    [HttpGet(Name = "GetCurl")]
+    public string Get()
+    {
+        return "";
+    }
+}
+
